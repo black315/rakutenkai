00c7e617b809ac0e76ab8f3414c7124b047743f2
apine-dev <apine.dev@gmail.com>
2019-09-11 14:10:07 +0900

    Remove unncessary code

--- src/main/java/com/rakuten/internship/controller/UserController.java
+++ src/main/java/com/rakuten/internship/controller/UserController.java
@@ -1,16 +1,12 @@
 package com.rakuten.internship.controller;
 
-import com.rakuten.internship.entity.Tag;
 import com.rakuten.internship.entity.User;
 import com.rakuten.internship.service.TagService;
 import com.rakuten.internship.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -24,15 +20,8 @@ public class UserController {
 
     @GetMapping("/")
     public String home(Model model){
-//        createTestData();
         model.addAttribute("tags", tagService.findTags());
         return "home";
-    }
-
-    private void createTestData() {
-        Tag n = new Tag();
-        n.setName("Hospital");
-        tagService.save(n);
     }
 
     @GetMapping("/search")
@@ -40,7 +29,6 @@ public class UserController {
 
         List<User> users = userService.findUsers();
         model.addAttribute("users", users);
-        System.out.println(users);
         //TODO: Input the right View name
         return "output";
     }
