9fbcafa3d47527453ef1e5f0d280ab9b9160d73f
apine-dev <apine.dev@gmail.com>
2019-09-11 13:58:19 +0900

    Add Tag entity and integration with user

--- src/main/java/com/rakuten/internship/controller/UserController.java
+++ src/main/java/com/rakuten/internship/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.rakuten.internship.controller;
 
+import com.rakuten.internship.entity.Tag;
 import com.rakuten.internship.entity.User;
+import com.rakuten.internship.service.TagService;
 import com.rakuten.internship.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +19,20 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private TagService tagService;
+
     @GetMapping("/")
     public String home(Model model){
+//        createTestData();
+        model.addAttribute("tags", tagService.findTags());
         return "home";
+    }
+
+    private void createTestData() {
+        Tag n = new Tag();
+        n.setName("Hospital");
+        tagService.save(n);
     }
 
     @GetMapping("/search")
@@ -26,6 +40,7 @@ public class UserController {
 
         List<User> users = userService.findUsers();
         model.addAttribute("users", users);
+        System.out.println(users);
         //TODO: Input the right View name
         return "output";
     }
