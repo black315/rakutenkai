0e643f7c0f562e8895e95d23df5f6548a4d56741
NaoyaIijima <naoyaiijima@hiroshima-u.ac.jp>
2019-09-13 08:38:39 +0900

    Add appointment lists

--- src/main/java/com/rakuten/internship/controller/AppointmentController.java
+++ src/main/java/com/rakuten/internship/controller/AppointmentController.java
@@ -1,23 +1,33 @@
 package com.rakuten.internship.controller;
 
 import com.rakuten.internship.entity.Appointment;
+import com.rakuten.internship.entity.User;
 import com.rakuten.internship.service.AppointmentService;
 import com.rakuten.internship.service.TagService;
 import com.rakuten.internship.service.UserService;
+import com.rakuten.internship.session.LogIn;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class AppointmentController {
 
     @Autowired
     private AppointmentService appointmentService;
+
+    @Autowired
     private TagService tagService;
+
+    @Autowired
     private UserService userService;
+
+    @Autowired
+    private LogIn login;
 
     @GetMapping("/appointment")
     public String appointment(Model model){
@@ -29,4 +39,27 @@ public class AppointmentController {
 
         return "appointment";
     }
+
+    @GetMapping("/appointmentList")
+    public String getAppointmentList(Model model){
+        System.out.println("hello");
+
+        Integer requesterId = login.getUserId();
+        System.out.println(requesterId);
+        if (requesterId == null){
+            model.addAttribute("user", new User());
+            return "sign_in";
+        }
+
+        List<Appointment> appointments = null;
+
+        User requester = userService.findUserById(requesterId);
+
+        appointments = appointmentService.findAppointmentsByRequester(requester);
+        model.addAttribute("appointments", appointments);
+
+        return "appointment_list";
+
+    }
+
 }
--- src/main/java/com/rakuten/internship/service/AppointmentService.java
+++ src/main/java/com/rakuten/internship/service/AppointmentService.java
@@ -1,12 +1,16 @@
 package com.rakuten.internship.service;
 
 import com.rakuten.internship.entity.Appointment;
+import com.rakuten.internship.entity.User;
 import com.rakuten.internship.repository.AppointmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.support.CustomSQLErrorCodesTranslation;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -17,5 +21,9 @@ public class AppointmentService {
 
     public List<Appointment> findAppointments(){
         return appointmentRepository.findAll();
+    }
+
+    public List<Appointment> findAppointmentsByRequester(User requester){
+        return appointmentRepository.findByRequester(requester);
     }
 }
