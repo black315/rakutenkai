f8f71f24d21884b468110dcdb4bcaf2bd341d389
NaoyaIijima <naoyaiijima@hiroshima-u.ac.jp>
2019-09-12 18:09:10 +0900

    Fix appointment page

--- src/main/java/com/rakuten/internship/controller/AppointmentController.java
+++ src/main/java/com/rakuten/internship/controller/AppointmentController.java
@@ -2,6 +2,8 @@ package com.rakuten.internship.controller;
 
 import com.rakuten.internship.entity.Appointment;
 import com.rakuten.internship.service.AppointmentService;
+import com.rakuten.internship.service.TagService;
+import com.rakuten.internship.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +16,8 @@ public class AppointmentController {
 
     @Autowired
     private AppointmentService appointmentService;
+    private TagService tagService;
+    private UserService userService;
 
     @GetMapping("/appointment")
     public String appointment(Model model){
