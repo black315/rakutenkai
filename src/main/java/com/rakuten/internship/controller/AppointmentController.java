720b7968f352da427d9d2effbfc30acd268802f4
NaoyaIijima <naoyaiijima@hiroshima-u.ac.jp>
2019-09-12 16:45:03 +0900

    Improve appointment page

--- src/main/java/com/rakuten/internship/controller/AppointmentController.java
+++ src/main/java/com/rakuten/internship/controller/AppointmentController.java
@@ -1,10 +1,13 @@
 package com.rakuten.internship.controller;
 
+import com.rakuten.internship.entity.Appointment;
 import com.rakuten.internship.service.AppointmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class AppointmentController {
@@ -14,6 +17,12 @@ public class AppointmentController {
 
     @GetMapping("/appointment")
     public String appointment(Model model){
+
+        List<Appointment> appointment;
+
+        appointment = appointmentService.findAppointments();
+        model.addAttribute("appointment", appointment);
+
         return "appointment";
     }
 }
