d64b13f10ba70fa1a768908ea2d8ae4b9a546d95
black <ryo.0301.1999@gmail.com>
2019-09-12 10:53:10 +0900

    Fix extra diff

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,17 @@
--- INSERT INTO lang(lang_id, lang_cap)
--- VALUES(1, 'Japanese'), (2, 'English'), (3, 'Italian'),
--- (4, 'French'), (5, 'Chinese'), (6, 'Korean'),
--- (7, 'Spanish'), (8, 'Hindi'), (9, 'Malay'),
+
+-- INSERT INTO lang(lang_id, lang_cap) 
+-- VALUES(1, 'Japanese'), (2, 'English'), (3, 'Italian'), 
+-- (4, 'French'), (5, 'Chinese'), (6, 'Korean'), 
+-- (7, 'Spanish'), (8, 'Hindi'), (9, 'Malay'), 
 -- (10, 'Russian'), (11, 'Portuguese');
+
 DROP TABLE IF EXISTS lang;
+
 CREATE TABLE IF NOT EXISTS lang(
-   lang_id INT(10) PRIMARY KEY AUTO_INCREMENT,
-   lang_cap VARCHAR(100)
+    lang_id INT(10) PRIMARY KEY AUTO_INCREMENT,
+    lang_cap VARCHAR(100)
 );
+
 INSERT INTO USER (NAME, COUNTRY, USERNAME, CITY, GENDER, PICTURE_URL, RATING,  ROLE, BIRTHDATE, SELF_INTRODUCTION, LATITUDE, LONGITUDE) VALUES
   ('John Crom', 'Japan', 'john.crom', 'Osaka', 'Male', 'url', 3, 'USER', '2000/07/10', 'About John', 35.66572, 139.73100),
   ('Carla Pike', 'United States of America', 'carla.pike', 'Osaka', 'Female', 'url', 3, 'USER', '2000/07/10', 'About Carla', 35.66572, 139.73100),
@@ -17,20 +21,21 @@ INSERT INTO USER (NAME, COUNTRY, USERNAME, CITY, GENDER, PICTURE_URL, RATING,  R
   ('Carl Times', 'Japan', 'carl.times', 'Nagoya', 'Male', 'url', 3, 'USER', '1998/07/10', 'Likes Languages and music', 35.66572, 139.73100);
 
 INSERT INTO TAG (NAME) VALUES
- ('Medical support'),
- ('Transportation'),
- ('Pension'),
- ('National Insurrance'),
- ('Translation'),
- ('Accomodation'),
- ('Ward office'),
- ('Immigration'),
- ('Moving'),
- ('Tourism'),
- ('Restaurants'),
- ('Religion'),
- ('Japanese Traditional Arts'),
- ('Real Estate');
+  ('Medical support'),
+  ('Transportation'),
+  ('Pension'),
+  ('National Insurrance'),
+  ('Translation'),
+  ('Accomodation'),
+  ('Ward office'),
+  ('Immigration'),
+  ('Moving'),
+  ('Tourism'),
+  ('Restaurants'),
+  ('Religion'),
+  ('Japanese Traditional Arts'),
+  ('Real Estate');
+
 INSERT INTO USER_TAGS (USER_ID, TAGS_TAG_ID) VALUES
     (1, 1),
     (1, 2),
