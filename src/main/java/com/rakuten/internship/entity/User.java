2c10382dfc6d5a0e97823e2d14480b19f3a797b6
bellis1120 <bellis1120@icloud.com>
2019-09-12 15:05:05 +0900

    Create log-in session

--- src/main/java/com/rakuten/internship/entity/User.java
+++ src/main/java/com/rakuten/internship/entity/User.java
@@ -43,6 +43,7 @@ public class User {
 	private List<Tag> tags;
 
 	private String country;
+	private String password; //This has to be encrypted
 	private String city;
 	private String username;
 	private String role;
