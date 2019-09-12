af54d83affa238877ae9b4136f6b63f385f7cf24
NaoyaIijima <naoyaiijima@hiroshima-u.ac.jp>
2019-09-12 09:35:36 +0900

    Change test data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,11 +13,11 @@ CREATE TABLE IF NOT EXISTS lang(
 );
 
 INSERT INTO USER (NAME, COUNTRY, USERNAME, CITY, GENDER, PICTURE_URL, RATING,  ROLE, BIRTHDATE, SELF_INTRODUCTION, LATITUDE, LONGITUDE) VALUES
-  ('John Crom', 'Japan', 'john.crom', 'Osaka', 'Male', 'url', 3, 'USER', '2000/07/10', 'About John', 35.66572, 139.73100),
-  ('Carla Pike', 'United States of America', 'carla.pike', 'Osaka', 'Female', 'url', 3, 'USER', '2000/07/10', 'About Carla', 35.66572, 139.73100),
-  ('Thomas Fenmoy', 'Vietnam', 'thomas.fenmoy', 'Osaka', 'Male', 'url', 3, 'USER', '2000/07/10', 'About Thomas', 35.66572, 139.73100),
-  ('Doraemon', 'Japan', 'anpan.man', 'Tokyo', 'Male', 'url', 3, 'USER', '2000/07/10', 'About Doraemon', 35.66572, 139.73100),
-  ('Gopher', 'USA', 'go.lang', 'LA', 'Male', 'url', 3, 'USER', '2000/07/10', 'About Gopher', 35.66572, 139.73100);
+  ('John Crom', 'Japan', 'john.crom', 'Osaka', 'Male', 'url', 3, 'USER', '2000/07/10', 'About John', 34.41, 135.3),
+  ('Carla Pike', 'United States of America', 'carla.pike', 'Osaka', 'Female', 'url', 3, 'USER', '2000/07/10', 'About Carla', 34.411, 135.301),
+  ('Thomas Fenmoy', 'Vietnam', 'thomas.fenmoy', 'Osaka', 'Male', 'url', 3, 'USER', '2000/07/10', 'About Thomas', 34.412, 135.302),
+  ('Doraemon', 'Japan', 'anpan.man', 'Tokyo', 'Male', 'url', 3, 'USER', '2000/07/10', 'About Doraemon', 35.66372, 139.73100),
+  ('Gopher', 'USA', 'go.lang', 'LA', 'Male', 'url', 3, 'USER', '2000/07/10', 'About Gopher', 35.66772, 139.73100);
 
 INSERT INTO TAG (NAME) VALUES
   ('Medical support'),
